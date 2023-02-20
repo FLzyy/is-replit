0905e4249613c660305bbf6f4404a97ac7562b5e
FLzyy <flzyy72@gmail.com>
2023-02-20 18:49:14 +0800

    create tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,37 @@
-/**
- * Testing:
- *
- * This file is what is used to test `index.ts` using the built in node:test module.
- *
- * Delete if requirements are unclear or frequently changes.
- */
-
+import { equal } from "node:assert/strict";
 import { describe, it } from "node:test";
+import isReplit from "../src/index";
+
+describe("not replit", () => {
+  it("should return false", () => {
+    equal(isReplit(), false);
+  });
+});
+
+describe("in replit", () => {
+  const envVar = [
+    "REPLIT",
+    "REPL_OWNER",
+    "REPLIT_DB_URL",
+    "REPL_ID",
+    "REPL_IMAGE",
+    "REPL_LANGUAGE",
+    "REPL_PUBKEYS",
+    "REPL_SLUG",
+    "PRYBAR_FILE",
+  ];
+
+  envVar.forEach((value) => {
+    it(`should return true (${value})`, () => {
+      process.env[value] = "1";
+
+      equal(isReplit(), true);
+
+      // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+      delete process.env[value];
+    });
+  });
+  it("should return false", () => {
+    equal(isReplit(), false);
+  });
+});

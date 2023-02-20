11862f3144b3c5ed9c0494c7f3dbeef649459ce9
FLzyy <flzyy72@gmail.com>
2023-02-20 18:38:13 +0800

    create function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
-/**
- * Index:
- *
- * Main entrypoint for your package.
- */
+const envVar = [
+  "REPL_OWNER",
+  "REPLIT_DB_URL",
+  "REPL_ID",
+  "REPL_IMAGE",
+  "REPL_LANGUAGE",
+  "REPL_PUBKEYS",
+  "REPL_SLUG",
+  "PRYBAR_FILE"
+]
+
+export default function isHeroku(): boolean {
+  for (let i = 0, n = envVar.length; i < n; i++) {
+    if (!(envVar[i] in process.env)) {
+      return false;
+    }
+  }
+
+  return true;
+}

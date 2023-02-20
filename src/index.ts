c2dabbdc517690185b3bf81073902af0456b3261
FLzyy <flzyy72@gmail.com>
2023-02-20 19:00:55 +0800

    simplify

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,3 @@
-const envVar = [
-  "REPLIT",
-  "REPL_OWNER",
-  "REPLIT_DB_URL",
-  "REPL_ID",
-  "REPL_IMAGE",
-  "REPL_LANGUAGE",
-  "REPL_PUBKEYS",
-  "REPL_SLUG",
-  "PRYBAR_FILE",
-];
-
 export default function isReplit(): boolean {
-  for (let i = 0, n = envVar.length; i < n; i++) {
-    if (envVar[i] in process.env) {
-      return true;
-    }
-  }
-  return false;
+  return "REPL_ID" in process.env || "REPLIT" in process.env;
 }
